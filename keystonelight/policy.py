f78793c798136b5683fa09e2760be0fb1d8a26ab
termie <github@anarkystic.com>
2011-11-21 15:31:52 -0800

    readmeware

--- keystonelight/policy.py
+++ keystonelight/policy.py
@@ -16,3 +16,50 @@ class Manager(object):
     def can_haz(self, context, target, credentials):
         """Check whether the given creds can perform action on target."""
         return self.driver.can_haz(target, credentials)
+
+    def add_rule(self, context, target, rule):
+        pass
+
+
+
+
+
+
+"""Brain format:
+
+    target:
+        nova:action:reboot_instance
+
+    rule:
+        roles:admin
+        roles:sysadmin, tenant_id:%(tenant_id)s
+
+
+    >>> can_haz(target='nova:action:reboot_instance',
+                extra={'tenant_id': object.project_id},
+                creds={'user_id': context.user.id,
+                       'tenant_id': context.tenant.id,
+                       'roles': ['role:%s' for x['id'] in context.roles]})
+
+
+    results in background operations
+
+    def _check_haz(target, extra, creds):
+        rules = brain.get_rule('nova:action:reboot_instance')
+        for rule in rules:
+            if self.check_rule(rule, extra, creds):
+                return True
+        return False
+
+    def _check_rule(rule, extra, creds):
+        if type(rule) is type(tuple()) or type(rule) is type(list()):
+            for x in rule:
+                if not self._check_rule(x, extra, creds):
+                    return False
+
+
+
+
+"""
+
+class
